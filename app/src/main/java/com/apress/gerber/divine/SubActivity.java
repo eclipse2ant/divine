1729b88092eeded88d7b1e39cfa7cfd337fc1958
hiroko <hiroko_jyosehutyen@yahoo.co.jp>
2016-10-25 03:28:16 +0900

    button kansei yeah!

---
+++ app/src/main/java/com/apress/gerber/divine/SubActivity.java
@@ -0,0 +1,20 @@
+package com.apress.gerber.divine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hiroko on 2016/10/25.
+ */
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+
+    }
+}
+
